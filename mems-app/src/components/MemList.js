7dd94cd1471fae1f917d23b4cdbe330dfde2c1e5
izaozga <izaozga@users.noreply.github.com>
2020-06-22 11:06:30 +0200

    Rewriting list

--- mems-app/src/components/MemList.js
+++ mems-app/src/components/MemList.js
@@ -8,73 +8,81 @@ import WomanAndClass from "../Gallery/WomanAndClass.jpg";
 
 
 
-const ListOfMems = (props) => {
 
-    return (
-        <li>
-            <div>
-                
-                <div>{props.title}</div>
-                <div>{props.img}</div>
-                
-            </div>
 
-            <button> Lubię to! </button>
-            <button> Nie lubię tego! </button>
-        </li>
-    )
-}
+
 
 class MemList extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     state = {
+
 
         elements: [
             {
                 title: "Piękny język polski",
                 img: <img alt="Wyraz dżdżownica w kilku językach" src={BeatifullPolishLanguage} />,
-                vote: 0
-                
+                votes: 0,
+
             },
             {
                 title: "Smuteczek :-(",
                 img: <img alt="Zakaz narzekania" src={Depression} />,
-                vote: 0,
-                
-            }, 
+                votes: 0
+
+
+            },
             {
                 title: "Oto jest pytanie!",
-                img:  <img alt="Kulfon sprawdza, co z nas wyrosło" src={Kulfon} />,
+                img: <img alt="Kulfon sprawdza, co z nas wyrosło" src={Kulfon} />,
                 vote: 0
             },
             {
                 title: "Już wiesz?",
-                img:  <img alt="Powód noszenia maseczki" src={Mask} />,
-                vote: 0
+                img: <img alt="Powód noszenia maseczki" src={Mask} />,
+                votes: 0
 
             },
             {
                 title: "Do przemyślenia!",
                 img: <img alt="Lista ważnych rzeczy, które są za darmo" src={TheBestThings} />,
-                vote: 0
+                votes: 0
 
             },
             {
                 title: "To jest kobieta z klasą!",
-                img:  <img alt="Portret kobiety z klasą" src={WomanAndClass} />,
-                vote: 0
+                img: <img alt="Portret kobiety z klasą" src={WomanAndClass} />,
+                votes: 0
             },
 
         ]
 
     }
+
+
     render() {
+        const photoMems = this.state.elements.map(e => {
+            return (
+                <ul key={e.title}>
+                    <div>{e.title}</div>
+                    <div>{e.img} </div>
+
+                    <div>
+                        <div><button>Lubię to!</button></div>
+                        <div>{e.votes}</div>
+                        <div><button>Nie lubię tego!</button></div>
+                    </div>
+                </ul>
+            )
+        })
+
         return (
-            <ul> 
-                {this.state.elements.map(listOfMems => <ListOfMems key = {listOfMems.title}
-                title = {listOfMems.title}
-                img = {listOfMems.img}
-                
-                />)}
+            <ul>
+                {photoMems}
+
             </ul>
         )
     }
