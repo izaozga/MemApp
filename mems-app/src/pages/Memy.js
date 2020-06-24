309d860bac7f05908c134417c46e19137352e173
izaozga <izaozga@users.noreply.github.com>
2020-06-24 23:18:33 +0200

    latest version

--- mems-app/src/pages/Memy.js
+++ mems-app/src/pages/Memy.js
@@ -1,20 +1,25 @@
 import React from "react";
 import MemList from "../components/memList";
-import BeatifullPolishLanguage from '../Gallery/BeatifullPolishLanguage.jpg'
-import Depression from '../Gallery/Depression.jpg'
-import Kulfon from '../Gallery/Kulfon.jpeg'
-import Mask from '../Gallery/Mask.jpg'
-import TheBestThings from '../Gallery/TheBestThings.jpg'
-import WomanAndClass from '../Gallery/WomanAndClass.jpg'
+
+
 class Memy extends React.Component {
 
   render() {
+
+
+
+
+
+
     return (
       <div>
         <h1> Poznajcie nasze memy! </h1>
+
+
         <MemList />
 
-      
+
+
       </div>)
   }
 }
